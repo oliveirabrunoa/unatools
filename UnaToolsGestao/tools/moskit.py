81508e5ea775a516e62078000a77065d339ec188
oliveirabrunoa <oliveirabrunoa@gmail.com>
2020-02-29 11:48:12 -0300

    Correção de erros na criação de contatos

--- UnaToolsGestao/tools/moskit.py
+++ UnaToolsGestao/tools/moskit.py
@@ -1,65 +1,23 @@
 import requests
 import json
-# import string
-# import random
-# from django.conf import settings
-# import base64
-# import os.path
-# from django.utils import timezone
-
 
 class MoskitObj(object):
 
     def __init__(self):#adicionar instancia lead
         self.apikey = 'c8e53183-f582-40be-8b5d-497871dee436'
         self.base_url= 'https://api.moskitcrm.com/v1/'
+        self.headers = {'content-type': "application/json",'apikey': "c8e53183-f582-40be-8b5d-497871dee436",'cache-control': "no-cache",}
 
     def criar_contato(self):
-        url = 'https://api.moskitcrm.com/v1/contacts'
-        headers = {'content-type': 'application/json', 'apikey': 'c8e53183-f582-40be-8b5d-497871dee436'}
+        url = self.base_url + "contacts"
+        payload = json.dumps({
+                                "name":"aaaaaaaaaaaaaaaBruno Teste Intesdsdsdgração",
+                                "notes":"Teste",
+                                "createdBy":{"id":"24453"},
+                                "responsible":{"id":"24453"},
+                                "emails":[{"address":"bbbbbbbstring@gmail.com",}],
+                                "phones": {"number":"888888888",}
+                            })
 
-        dados =  {
-                    "name":"aaaaaaaaaaaaaaaBruno Teste Intesdsdsdgração",
-                    "notes":"Teste",
-                    "createdBy":{
-                          "id":"24453"
-                       },
-                    "responsible":{
-                          "id":"24453"
-                       },
-                    "emails":[
-                          {
-                             "address":"bbbbbbbstring@gmail.com",
-                          }
-                       ],
-                    "phones":
-                          {
-                             "number":"888888888",
-                          }
-                    }
-
-        response = requests.post(url, json = json.dumps(dados), headers=headers, verify = False)
-        print('Moskit criar_contato',response.text)
-        return response.text
-
-
-    # def check_arquivo(self, path_contrato):
-    #     return os.path.exists(path_contrato)
-    #
-    #
-    # def gerar_doc_base64(self, path_contrato):
-    #     if self.check_arquivo(path_contrato):
-    #         try:
-    #             with open(path_contrato, "rb") as pdf_file:
-    #                 encoded_string = base64.b64encode(pdf_file.read())
-    #                 return encoded_string.decode('utf-8')
-    #         except Exception as e:
-    #             print(e.message, e.args)
-    #     return False
-    #
-    #
-    # def get_prazo_assinatura(self, dias):
-    #     now = timezone.now()
-    #     prazo = now + timezone.timedelta(days=dias)
-    #     prazo_utc = prazo.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
-    #     return '{0}{1}'.format(prazo_utc,'Z')
+        response = requests.post(url, data=payload, headers=self.headers)
+        print(response.text)
