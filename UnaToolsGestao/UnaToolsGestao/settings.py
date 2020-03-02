444de4764a229a1f35e18b49dee1d2e807e16a12
oliveirabrunoa <oliveirabrunoa@gmail.com>
2020-03-02 18:36:42 -0300

    Alteração do Banco de Dados

--- UnaToolsGestao/UnaToolsGestao/settings.py
+++ UnaToolsGestao/UnaToolsGestao/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'UnaToolsGestao.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'unatools$dbuna',
+       'USER': 'unatools',
+       'PASSWORD': 'A6901804',
+       'HOST': 'unatools.mysql.pythonanywhere-services.com',
+   }
+ }
 
 
 # Password validation
