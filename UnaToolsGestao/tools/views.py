5c58d38023746d74524ac5fe027a2ce0fd36cbb6
oliveirabrunoa <oliveirabrunoa@gmail.com>
2020-06-27 20:59:46 -0300

    Criação de views para confirmação de dados do contrato

--- UnaToolsGestao/tools/views.py
+++ UnaToolsGestao/tools/views.py
@@ -7,6 +7,8 @@ from django.shortcuts import render
 from django.views import View
 from .models import Contrato
 from .forms import ContratoFormAdmin, ContratoFormAdmin2
+from django.utils import timezone
+import datetime
 
 
 def visualizar_contrato(request, param):
@@ -65,29 +67,34 @@ class confirmar_dados(View):
         if not contrato:
             return HttpResponseRedirect('consultar_cliente')
 
-        initial_data = {'contratante': contrato.contratante, 'email': contrato.email,
-                        'rg': contrato.rg, 'cpf': contrato.cpf,'endereco': contrato.endereco,
-                        'cidade_estado': contrato.cidade_estado, 'cep': contrato.cep,
-                        'telefone': contrato.telefone, 'data_nascimento': contrato.data_nascimento}
-
-        form = self.form_class(initial= initial_data)
-        return render(request, self.template_name, { 'form' : form, 'contratante': contrato.contratante, 'email': contrato.email,
+        return render(request, self.template_name, { 'form' : self.form_class(), 'contratante': contrato.contratante, 'email': contrato.email,
                         'rg': contrato.rg, 'cpf': contrato.cpf,'endereco': contrato.endereco,
                         'cidade_estado': contrato.cidade_estado, 'cep': contrato.cep,
                         'telefone': contrato.telefone, 'data_nascimento': contrato.data_nascimento})
 
-
-    #PARA REFATORAR - BASE
     def post(self, request, *args, **kwargs):
         print(request.POST)
-        initial_data=dict(email=request.POST['email'])
-        form = ContratoFormAdmin2(initial_data)
-        print(form)
-        if form.is_valid():
-            print('passou validação')
+        contrato=Contrato.objects.filter(id=request.session.get('contrato_id')).first()
+
+        if request.POST:
+            contrato_atualizado=Contrato.objects.filter(id=request.session.get('contrato_id')).update(email=contrato.email,
+                              contratante = request.POST['contratante'],
+                              rg=request.POST['rg'], cpf=request.POST['cpf'],
+                              endereco=request.POST['endereco'],
+                              cidade_estado=request.POST['cidade-estado'],
+                              cep=request.POST['cep'], telefone=request.POST['telefone'],
+                              data_nascimento=data_nasc_format(request.POST['data_nascimento'])) #VERIFICAR VALIDAÇÃO
+
             return HttpResponseRedirect('confirmar_servico')
         return render(request, 'index.html')
 
+def data_nasc_format(data_nasc):
+    data_nasc_cliente = data_nasc
+    if data_nasc_cliente.find('/')  > 0:
+        return datetime.datetime.strptime(data_nasc_cliente,"%d/%m/%Y").strftime("%Y-%m-%d")
+    elif data_nasc_cliente.find('-')  > 0:
+        return datetime.datetime.strptime(data_nasc_cliente,"%d-%m-%Y").strftime("%Y-%m-%d")
+    return None
 
 class confirmar_servico(View):
     def get(self, request,*args, **kwargs):
