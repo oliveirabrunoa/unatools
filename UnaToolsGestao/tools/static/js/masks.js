92eab8fc85a0bd18e48f76106e39820e0dfc43aa
oliveirabrunoa <oliveirabrunoa@gmail.com>
2020-06-27 20:13:56 -0300

    Configuração de formulários

--- UnaToolsGestao/tools/static/js/masks.js
+++ UnaToolsGestao/tools/static/js/masks.js
@@ -5,6 +5,6 @@ $(document).ready(function() {
     $('.phone-mask').mask('(00) 00000-0000');
     $('.cpf-mask').mask('000.000.000-00');
     $('.datepicker').datepicker({
-        dateFormat: "dd/mm/yy",
+        dateFormat: "dd/mm/YYYY",
     });
 });
--- UnaToolsGestao/tools/views.py
+++ UnaToolsGestao/tools/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 import requests
 from django.shortcuts import render
 from django.views import View
+from .models import Contrato
 from .forms import ContratoFormAdmin, ContratoFormAdmin2
 
 
@@ -34,7 +35,7 @@ def gerar_contrato(nome_cliente):
 def index(request):
     return render(request, 'Modelo-de-Contrato-PPC-ONLINE html version.html',{'contratante':'Bruno Araújo de Oliveira'})
 
-
+from django.contrib import messages
 class consultar_cliente(View):
     template_name = 'index_base.html'
     form_class = ContratoFormAdmin
@@ -44,24 +45,37 @@ class consultar_cliente(View):
         return render(request, self.template_name, { 'form' : form})
 
     def post(self, request, *args, **kwargs):
-        print(request.POST)
-        initial_data=dict(email=request.POST['email'])
-        form = ContratoFormAdmin2(initial_data)
-        print(form)
-        if form.is_valid():
-            print('passou validação')
-        #     post = form.save(commit=False)
-        #     cliente= get_cliente_by_email(post.email)
-        #     cliente.nome = post.nome
-        #     cotacao.save()
-        #
-        #     request.session['cotacao_id']= str(cotacao.id)
-            return HttpResponseRedirect('confirmar_dados')
-        return render(request, 'index.html')
+        param = request.POST['email']
+        if param:
+            if Contrato.objects.filter(email=param).first():
+                request.session['contrato_id']= str(Contrato.objects.filter(email=param).first().id)
+                return HttpResponseRedirect('confirmar_dados')
+            else:
+                return render(request, self.template_name, {'messages' : 'messages', 'email':param})
+        return render(request, self.template_name, { 'form' : self.form_class()})
+
 
 class confirmar_dados(View):
+    template_name = 'dados-contrato.html'
+    form_class = ContratoFormAdmin
+
     def get(self, request,*args, **kwargs):
-        return render(request, 'dados-contrato.html')
+        contrato=Contrato.objects.filter(id=request.session.get('contrato_id')).first()
+
+        if not contrato:
+            return HttpResponseRedirect('consultar_cliente')
+
+        initial_data = {'contratante': contrato.contratante, 'email': contrato.email,
+                        'rg': contrato.rg, 'cpf': contrato.cpf,'endereco': contrato.endereco,
+                        'cidade_estado': contrato.cidade_estado, 'cep': contrato.cep,
+                        'telefone': contrato.telefone, 'data_nascimento': contrato.data_nascimento}
+
+        form = self.form_class(initial= initial_data)
+        return render(request, self.template_name, { 'form' : form, 'contratante': contrato.contratante, 'email': contrato.email,
+                        'rg': contrato.rg, 'cpf': contrato.cpf,'endereco': contrato.endereco,
+                        'cidade_estado': contrato.cidade_estado, 'cep': contrato.cep,
+                        'telefone': contrato.telefone, 'data_nascimento': contrato.data_nascimento})
+
 
     #PARA REFATORAR - BASE
     def post(self, request, *args, **kwargs):
